34b2d962cea26576fb7787b3d0a786387ed598c9
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-03-15 11:39:13 +0200

    com10

--- js/modal_buy.js
+++ js/modal_buy.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal_buy: document.querySelector("[data-modal-buy]"),
+    openModalBtn: document.querySelector("[data-modal-w-open]"),
+    closeModalBtn: document.querySelector("[data-modal-w-close]"),
+    modal: document.querySelector("[data-modal-w]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal2)
+  refs.closeModalBtn.addEventListener("click", toggleModal2)
 
-  function toggleModal() {
-    refs.modal_buy.classList.toggle("is-hidden");
+  function toggleModal2() {
+    refs.modal.classList.toggle('is-hidden-w')
   }
 })();
---
+++ js/submit.js
@@ -0,0 +1,13 @@
+(() => {
+     const applicantForm = document.getElementById('form')
+    applicantForm.addEventListener('submit', handleFormSubmit)
+    
+function handleFormSubmit(event) {
+  
+    event.toggleModal2()
+
+}
+
+	
+})();
+		
